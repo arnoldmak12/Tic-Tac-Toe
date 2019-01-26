365af8c32b80323610f95b6ca5e9b16e91cd7f7f
arnoldmak12 <43900627+arnoldmak12@users.noreply.github.com>
2019-01-26 15:45:01 -0500

    Add files via upload

---
+++ PieceX.java
@@ -0,0 +1,34 @@
+public class PieceX extends Piece {
+
+    private String side;
+    private int x;
+    private int y;
+
+    public PieceX() {
+
+    }
+
+    private void setSide(String s) {
+        this.side = s;
+    }
+
+    private void setX(int X) {
+        this.x = X;
+    }
+
+    private void setY(int Y) {
+        this.y = Y;
+    }
+
+    private String getSide() {
+        return this.side;
+    }
+
+    private int getX() {
+        return this.x;
+    }
+
+    private int getY() {
+        return this.y;
+    }
+}
