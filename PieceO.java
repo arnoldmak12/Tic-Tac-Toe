73d2518d8a56bd40350640166a94467fb25c266a
arnoldmak12 <43900627+arnoldmak12@users.noreply.github.com>
2019-01-26 15:45:54 -0500

    Add files via upload

---
+++ PieceO.java
@@ -0,0 +1,34 @@
+public class PieceO extends Piece {
+
+    private String side;
+    private int x;
+    private int y;
+
+    public PieceO() {
+
+    }
+
+    private void setSide(String s) {
+        this.side = s;
+    }
+
+    private void setX(int X) {
+        this.x = X;
+    }
+
+    private void setY(int Y) {
+        this.y = Y;
+    }
+
+    private String getSide() {
+        return this.side;
+    }
+
+    private int getX() {
+        return this.x;
+    }
+
+    private int getY() {
+        return this.y;
+    }
+}
