61d55171b86e0f8993ff8558bc48f154fe6c6650
Rocksrock18 <46852825+Rocksrock18@users.noreply.github.com>
2019-01-26 15:23:46 -0500

    Update Human.java

--- Human.java
+++ Human.java
@@ -1,15 +1,36 @@
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
 public class Human extends Player {
 
     public Human() {
-
+    	
     }
 
-    @Override
     public Square makeMove() {
-
+    	return new Square();
     }
+    
+    private class MyMouseAdapter extends MouseAdapter {
+    	
+        @Override
+        public void mousePressed(MouseEvent e) {
+        	
+        }
 
-    public void mousePressed(MouseEvent e) {
+        @Override
+        public void mouseDragged(MouseEvent e) {
+            
+        }
 
+        @Override
+        public void mouseReleased(MouseEvent e) {
+        	
+        }
+
+        public void updateLocation(MouseEvent e) {
+        	
+           
+        }
     }
 }
