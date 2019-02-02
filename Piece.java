a7242ec7238671710bc7da94fc424239cd5eeffb
Joticaba <47064028+Joticaba@users.noreply.github.com>
2019-02-02 18:46:50 -0500

    Update Piece.java

--- Piece.java
+++ Piece.java
@@ -1,27 +1,7 @@
 public abstract class Piece {
 
-    
-    private int r;
-    private int c;
-
-    public Piece(int R,int C) {
-        r = R;
-        c = C;
-    }
-   
-   /**Returns the row of the piece
-    *
-    * @return row number of piece
-    */
-    public int getR() {
-        return r;
-    }
-    /**Returns the column of the piece
-    *
-    * @return column number of piece
-    */
-    public int getC() {
-        return c;
+	public Piece() {
+		
     }
 }
 
