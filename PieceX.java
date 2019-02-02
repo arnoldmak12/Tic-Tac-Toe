b588edba459569f77c532f6fcc8bc4a64930df71
arnoldmak12 <43900627+arnoldmak12@users.noreply.github.com>
2019-02-02 16:45:36 -0500

    Update PieceX.java

--- PieceX.java
+++ PieceX.java
@@ -1,12 +1,9 @@
 public class PieceX extends Piece {
 
-    private String side;
     private int x;
     private int y;
 
-    public PieceX(int X, int Y, String Side) {
-    	super(X, Y, Side);
+    public PieceX(int X, int Y) {
+        super(X, Y);
     }
-
-    
 }
