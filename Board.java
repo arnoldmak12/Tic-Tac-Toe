fb7b405b05382b0266ea6447ea8567cc8989b1b6
Joticaba <47064028+Joticaba@users.noreply.github.com>
2019-02-02 18:35:19 -0500

    Update Board.java

--- Board.java
+++ Board.java
@@ -3,16 +3,16 @@ public class Board {
     private Square[][] squares;
 
     public Board() {
-        this.squares = new Square[3][3];
+        squares = new Square[3][3];
     }
 
     /**
      * Checks if square is empty or not
-     * 
+     *
      * @param r
      *            either 0, 1, or 2. Represents the row of the 2D array.
      * @param c
-     *            either 0, 1, or 2. Represents the column of the 2D array
+     *            either 0, 1, or 2. Represents the column of the 2D array.
      * @return true for empty square
      * @return false for square with piece
      */
@@ -22,7 +22,7 @@ public class Board {
 
     /**
      * Places piece with the given type at the given square.
-     * 
+     *
      * @param piece
      *            will either be piece x or piece o.
      * @param r
@@ -31,6 +31,6 @@ public class Board {
      *            either 0, 1, or 2. Represents the column of the 2D array
      */
     public void movePiece(Piece piece, int r, int c) {
-    	squares[r][c].setPiece(piece);
+        squares[r][c].setPiece(piece);
     }
 }
