e202495896060b3a17d452a606c73fc473d32ce3
arnoldmak12 <43900627+arnoldmak12@users.noreply.github.com>
2019-02-02 16:45:21 -0500

    Update PieceO.java

--- PieceO.java
+++ PieceO.java
@@ -1,12 +1,9 @@
 public class PieceO extends Piece {
 
-    private String side;
     private int x;
     private int y;
 
-    public PieceO(int X, int Y, String Side) {
-    	super(X,Y,Side);
+    public PieceO(int X, int Y) {
+        super(X, Y);
     }
-
-    
 }
