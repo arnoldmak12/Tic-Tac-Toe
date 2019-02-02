89ab4616157fe45f2cc0e98fd28d0520c527c5d2
Joticaba <47064028+Joticaba@users.noreply.github.com>
2019-02-02 18:42:38 -0500

    Update Board.java

--- Board.java
+++ Board.java
@@ -13,8 +13,7 @@ public class Board {
      *            either 0, 1, or 2. Represents the row of the 2D array.
      * @param c
      *            either 0, 1, or 2. Represents the column of the 2D array.
-     * @return true for empty square
-     * @return false for square with piece
+     * @return true for empty square, false for square with piece
      */
     public boolean isEmpty(int r, int c) {
         return squares[r][c].getPiece() == null;
