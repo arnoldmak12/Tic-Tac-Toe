7fa76d6a2ff096b4e2189045f79c936c66a2d015
arnoldmak12 <43900627+arnoldmak12@users.noreply.github.com>
2019-02-09 16:17:22 -0500

    Update TicTacToeGame.java

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class TicTacToeGame {
@@ -15,8 +16,33 @@ public class TicTacToeGame {
     	Board board = new Board();
         Player player1 = new Human();
         Player player2 = new Computer();
-    	Map moveHistory = new HashMap();
+    	Map<Player, List<int[]>> moveHistory = new HashMap<Player, List<int[]>>();
     	Piece o = new PieceO();
-    	board.movePiece(o, 1, 1);
+    	int[] move = new int[2];
+    	
+    	while(board.checkResult().equals("Not Done")) {
+    		
+    		//Player1's Turn
+    		move = player1.makeMove();		
+    		while(!board.isEmpty(move[0], move[1])){
+    			move = player1.makeMove();
+    		}
+    		board.movePiece(new PieceX(), move[0], move[1]); 		
+    		board.showBoard();
+    		
+    		if(!board.checkResult().equals("Not Done")) {
+    			break;
+    		}
+    		
+    		//Player2's Turn
+    		move = player2.makeMove();		
+    		while(!board.isEmpty(move[0], move[1])){
+    			move = player2.makeMove();
+    		}
+    		board.movePiece(new PieceO(), move[0], move[1]);    		
+    		board.showBoard();   		
+    	}
+    	
+    	System.out.println(board.checkResult());
     }
 }
