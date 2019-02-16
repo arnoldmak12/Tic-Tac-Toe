3b692d6cdb461ba17db2d485af88e48ef0b35873
arnoldmak12 <43900627+arnoldmak12@users.noreply.github.com>
2019-02-16 17:26:14 -0500

    Add files via upload

---
+++ gameView.java
@@ -0,0 +1,71 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.GridBagLayout;
+import java.awt.CardLayout;
+import javax.swing.BoxLayout;
+import javax.swing.SpringLayout;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import java.awt.Font;
+
+public class gameView extends javax.swing.JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					gameView frame = new gameView();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public gameView() {
+		setTitle("Tic-Tac-Toe");
+		setDefaultCloseOperation(gameView.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1200, 800);
+		contentPane = new JPanel();
+		contentPane.setBackground(Color.BLACK);
+		contentPane.setBorder(new EmptyBorder(0, 0, 0, 0));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.BLACK);
+		panel.setBounds(300, 140, 600, 600);
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setForeground(Color.BLACK);
+		lblNewLabel.setIcon(new ImageIcon("D:\\TicTacToe\\Tic-Tac-Toe Grid.png"));
+		lblNewLabel.setBounds(0, 0, 600, 600);
+		panel.add(lblNewLabel);
+		
+		JLabel lblPlayerTurn = new JLabel("Player Turn:");
+		lblPlayerTurn.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		lblPlayerTurn.setForeground(Color.WHITE);
+		lblPlayerTurn.setBounds(300, 69, 238, 58);
+		contentPane.add(lblPlayerTurn);
+	}
+}
