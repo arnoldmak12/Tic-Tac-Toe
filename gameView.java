f36432f5d336cf511fedcf2a1faad6b1f50f3f79
arnoldmak12 <43900627+arnoldmak12@users.noreply.github.com>
2019-02-16 18:24:20 -0500

    Update gameView.java

--- gameView.java
+++ gameView.java
@@ -16,6 +16,8 @@ import com.jgoodies.forms.layout.RowSpec;
 import javax.swing.JLabel;
 import javax.swing.ImageIcon;
 import java.awt.Font;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class gameView extends javax.swing.JFrame {
 
@@ -61,6 +63,96 @@ public class gameView extends javax.swing.JFrame {
 		lblNewLabel.setIcon(new ImageIcon(this.getClass().getResource("Tic-Tac-Toe Grid.png")));
 		lblNewLabel.setBounds(0, 0, 600, 600);
 		panel.add(lblNewLabel);
+		
+		JLabel square00 = new JLabel("");
+		square00.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				square00.setIcon(new ImageIcon(this.getClass().getResource("PieceO.png")));
+			}
+		});
+		square00.setBounds(0, 0, 174, 180);
+		panel.add(square00);
+		
+		JLabel square01 = new JLabel("");
+		square01.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				square01.setIcon(new ImageIcon(this.getClass().getResource("PieceO.png")));
+			}
+		});
+		square01.setBounds(214, 0, 174, 180);
+		panel.add(square01);
+		
+		JLabel square02 = new JLabel("");
+		square02.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				square02.setIcon(new ImageIcon(this.getClass().getResource("PieceX.png")));
+			}
+		});
+		square02.setBounds(426, 0, 174, 180);
+		panel.add(square02);
+		
+		JLabel square10 = new JLabel("");
+		square10.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				square10.setIcon(new ImageIcon(this.getClass().getResource("PieceO.png")));
+			}
+		});
+		square10.setBounds(0, 212, 174, 180);
+		panel.add(square10);
+		
+		JLabel square11 = new JLabel("");
+		square11.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				square11.setIcon(new ImageIcon(this.getClass().getResource("PieceX.png")));
+			}
+		});
+		square11.setBounds(214, 212, 174, 180);
+		panel.add(square11);
+		
+		JLabel square12 = new JLabel("");
+		square12.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				square12.setIcon(new ImageIcon(this.getClass().getResource("PieceX.png")));
+			}
+		});
+		square12.setBounds(426, 212, 174, 180);
+		panel.add(square12);
+		
+		JLabel square20 = new JLabel("");
+		square20.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				square20.setIcon(new ImageIcon(this.getClass().getResource("PieceO.png")));
+			}
+		});
+		square20.setBounds(0, 420, 174, 180);
+		panel.add(square20);
+		
+		JLabel square21 = new JLabel("");
+		square21.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				square21.setIcon(new ImageIcon(this.getClass().getResource("PieceX.png")));
+			}
+		});
+		square21.setBounds(214, 420, 174, 180);
+		panel.add(square21);
+		
+		JLabel square22 = new JLabel("");
+		square22.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				square22.setIcon(new ImageIcon(this.getClass().getResource("PieceO.png")));
+			}
+		});
+		square22.setBounds(426, 420, 174, 180);
+		panel.add(square22);
 		
 		JLabel lblPlayerTurn = new JLabel("Player Turn:");
 		lblPlayerTurn.setFont(new Font("Tahoma", Font.PLAIN, 24));
