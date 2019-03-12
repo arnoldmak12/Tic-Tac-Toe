c2cfc97e451cbf103ae5f2c32c0d51c20d2776ef
Rocksrock18 <46852825+Rocksrock18@users.noreply.github.com>
2019-03-12 17:01:31 -0400

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,22 +1,4 @@
-import java.util.Scanner;
-
-public class Player {
-
-    private String name;
-
-    public Player() {
-
-    }
-
-    public int[] makeMove() {
-    	Scanner sc = new Scanner(System.in);
-    	
-    	System.out.println("r?");
-    	int r = sc.nextInt();
-    	
-    	System.out.println("c?");
-    	int c = sc.nextInt();
-    	
-        return new int[] {r,c};
-    }
+interface Player {
+	String name = "";
+    public int[] makeMove();
 }
