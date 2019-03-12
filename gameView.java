6f61ff2c42ac27bc40ce080741f01d3e08346376
Rocksrock18 <46852825+Rocksrock18@users.noreply.github.com>
2019-03-12 16:55:27 -0400

    Update gameView.java

--- gameView.java
+++ gameView.java
@@ -18,6 +18,13 @@ import javax.swing.ImageIcon;
 import java.awt.Font;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.event.MenuKeyListener;
+import javax.swing.event.MenuKeyEvent;
 
 public class gameView extends javax.swing.JFrame {
 
@@ -25,6 +32,7 @@ public class gameView extends javax.swing.JFrame {
 	private int[] inputMove;
 	private JLabel[][] squares;
 	private JPanel panel;
+	private TicTacToeGame game;
 
 	/**
 	 * Launch the application.
@@ -45,10 +53,77 @@ public class gameView extends javax.swing.JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public gameView() {
+	public gameView(TicTacToeGame game1) {
 		setTitle("Tic-Tac-Toe");
 		setDefaultCloseOperation(gameView.EXIT_ON_CLOSE);
 		setBounds(100, 100, 1200, 800);
+		
+		game = game1;
+		
+		JMenuBar menuBar = new JMenuBar();
+		menuBar.setBackground(Color.BLACK);
+		setJMenuBar(menuBar);
+		
+		JMenu mnNew = new JMenu("New Game");
+		mnNew.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				mnNew.setForeground(Color.CYAN);
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				mnNew.setForeground(Color.WHITE);
+			}
+		});
+		mnNew.setForeground(Color.WHITE);
+		mnNew.setBackground(Color.BLACK);
+		menuBar.add(mnNew);
+		
+		JMenuItem mntmPlayerVsPlayer = new JMenuItem("Player vs. Player");
+		mntmPlayerVsPlayer.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				game.setPlayers("Player vs. Player");
+				setMove(3,3);			
+			}
+		});
+		mntmPlayerVsPlayer.setForeground(Color.WHITE);
+		mntmPlayerVsPlayer.setBackground(Color.BLACK);
+		mnNew.add(mntmPlayerVsPlayer);
+		
+		JMenuItem mntmPlayerVsComputer = new JMenuItem("Player vs. Computer");
+		mntmPlayerVsComputer.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				game.setPlayers("Player vs. Computer");
+				setMove(3,3);
+				
+			}
+		});
+		mntmPlayerVsComputer.setForeground(Color.WHITE);
+		mntmPlayerVsComputer.setBackground(Color.BLACK);
+		mnNew.add(mntmPlayerVsComputer);
+		
+		JMenuItem mntmComputerVsPlayer = new JMenuItem("Computer vs. Player");
+		mntmComputerVsPlayer.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				game.setPlayers("Computer vs. Player");
+				setMove(3,3);
+				
+			}
+		});
+		mntmComputerVsPlayer.setForeground(Color.WHITE);
+		mntmComputerVsPlayer.setBackground(Color.BLACK);
+		mnNew.add(mntmComputerVsPlayer);
+		
+		JMenuItem mntmComputerVsComputer = new JMenuItem("Computer vs. Computer");
+		mntmComputerVsComputer.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				game.setPlayers("Computer vs. Computer");
+				setMove(3,3);
+			}
+		});
+		mntmComputerVsComputer.setForeground(Color.WHITE);
+		mntmComputerVsComputer.setBackground(Color.BLACK);
+		mnNew.add(mntmComputerVsComputer);
 		contentPane = new JPanel();
 		contentPane.setBackground(Color.BLACK);
 		contentPane.setBorder(new EmptyBorder(0, 0, 0, 0));
@@ -231,13 +306,15 @@ public class gameView extends javax.swing.JFrame {
 			System.out.println("Hey buddy you set an invalid");
 		}
 	}
-	public void setWinner(String p)
+	public void clearScreen()
 	{
-		JLabel winner = new JLabel("");
-		winner.setForeground(Color.BLACK);
-		winner.setIcon(new ImageIcon(this.getClass().getResource(p)));
-		winner.setBounds(0, 0, 600, 600);
-		panel.add(winner);
+		for(int i = 0; i < 3; i++)
+		{
+			for(int k = 0; k < 3; k++)
+			{
+				squares[i][k].setIcon(null);
+			}
+		}
 	}
-	
+
 }
