bbcabd2598bc2572c67863840968d9936a8bc77e
Rocksrock18 <46852825+Rocksrock18@users.noreply.github.com>
2019-03-12 16:53:03 -0400

    Update Computer.java

--- Computer.java
+++ Computer.java
@@ -1,6 +1,15 @@
-public class Computer extends Player {
+public class Computer implements Player {
+	private gameView frame;
 
-    public Computer() {
+    public Computer(gameView frame1) {
+    	frame = frame1;
 
+    }
+    
+    public int[] makeMove()
+    {
+    	int r = (int)(Math.random()*3);
+    	int c = (int)(Math.random()*3);
+    	return new int[] {r,c};
     }
 }
