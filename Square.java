35a662181bc7dea48e0a2d05427e7c80011e2f5e
Rocksrock18 <46852825+Rocksrock18@users.noreply.github.com>
2019-03-30 17:01:37 -0400

    Update Square.java

--- Square.java
+++ Square.java
@@ -1,3 +1,4 @@
+//Class for each individual square on the board
 public class Square {
 
     private Piece piece;
@@ -6,10 +7,19 @@ public class Square {
         piece = null;
     }
 
+    /**
+     * Sets the piece for a specific square
+     * @param p
+     * The piece being placed on the square
+     */
     public void setPiece(Piece p) {
         piece = p;
     }
 
+    /**
+     * Gets the piece at the given square
+     * @return the piece at the given square
+     */
     public Piece getPiece() {
         return piece;
     }
